b4feb861d1aad73667253d53589e567b50af067d
akash <akasheldo@hotmail.com>
2020-10-11 10:04:15 -0400

    Installed prettier

--- src/hooks/getEntry.tsx
+++ src/hooks/getEntry.tsx
@@ -1,41 +1,42 @@
-import {gql, QueryResult, useQuery} from "@apollo/client";
+import { gql, QueryResult, useQuery } from "@apollo/client";
 
 const ENTRY = gql`
   query EntryQuery($id: ID!) {
     entry(id: $id) {
-      id,
-      term,
-      pos,
-      definitions,
-      antonyms,
-      synonyms,
+      id
+      term
+      pos
+      definitions
+      antonyms
+      synonyms
       examples {
-        sentence,
+        sentence
         translation
       }
-      regular,
+      regular
       note
     }
-  }`;
+  }
+`;
 
-type Entry =  {
-  id: string,
-  term: string,
-  pos: string,
-  definitions: string[],
-  antonyms?: string[],
-  synonyms?: string[],
+type Entry = {
+  id: string;
+  term: string;
+  pos: string;
+  definitions: string[];
+  antonyms?: string[];
+  synonyms?: string[];
   examples?: {
-    sentence: string,
-    translation: string,
-  }[],
-  regular?: boolean,
-  note?: string,
-}
+    sentence: string;
+    translation: string;
+  }[];
+  regular?: boolean;
+  note?: string;
+};
 
 type GetEntryResponse = {
-  entry: Entry
-}
+  entry: Entry;
+};
 
 export default function getEntry(id: string): QueryResult<GetEntryResponse> {
   return useQuery<GetEntryResponse>(ENTRY, { variables: { id: id } });
