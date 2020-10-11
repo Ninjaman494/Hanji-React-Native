eaf7791059ac3a51bac12d007754413f226b50f6
akash <akasheldo@hotmail.com>
2020-10-11 09:58:40 -0400

    Added Apollo Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import {Button, Text} from "react-native-paper";
+import getEntry from "./hooks/getEntry";
 
 export default function Index() {
+  const { loading, error, data } = getEntry('가다0');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {loading && <Text>Loading</Text>}
+      {error && <Text>Error</Text>}
+      {data && <Text>{data.entry.term}</Text>}
       <Button mode="contained" onPress={() => {}}>
         click me
       </Button>
