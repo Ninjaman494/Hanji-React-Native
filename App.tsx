dad0458a31baee831caa919361662fada246c6fe
akash <akasheldo@hotmail.com>
2020-10-11 09:58:40 -0400

    Added Apollo Client

--- App.tsx
+++ App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import Index from "./src/index";
 import {StatusBar} from "expo-status-bar";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://hanji-server.appspot.com/graphql',
+  cache: new InMemoryCache()
+});
 
 export default function App() {
   return (
-    <PaperProvider>
-      <Index/>
-      <StatusBar style="auto" />
-    </PaperProvider>
+    <ApolloProvider client={client}>
+      <PaperProvider>
+        <Index/>
+        <StatusBar style="auto" />
+      </PaperProvider>
+    </ApolloProvider>
   );
 }
