e97e08d3cb02be444e0d13aee73a96ca675cca5b
akash <akasheldo@hotmail.com>
2020-10-15 18:01:25 -0400

    Added conjugations

---
+++ src/hooks/useConjugations.tsx
@@ -0,0 +1,79 @@
+import { gql, QueryHookOptions, QueryResult, useQuery } from "@apollo/client";
+
+const CONJUGATIONS = gql`
+  query ConjugationsQuery(
+    $stem: String!
+    $isAdj: Boolean!
+    $honorific: Boolean!
+    $regular: Boolean
+    $conjugations: [String]
+  ) {
+    conjugations(
+      stem: $stem
+      isAdj: $isAdj
+      honorific: $honorific
+      regular: $regular
+      conjugations: $conjugations
+    ) {
+      name
+      conjugation
+      type
+      tense
+      speechLevel
+      honorific
+      pronunciation
+      romanization
+      reasons
+    }
+  }
+`;
+
+export type Conjugation = {
+  name: string;
+  conjugation: string;
+  type: string;
+  tense: "PRESENT" | "PAST" | "FUTURE" | "NONE";
+  speechLevel: "INFORMAL_LOW" | "INFORMAL_HIGH" | "FORMAL_LOW" | "FORMAL_HIGH";
+  honorific: boolean;
+  pronunciation: boolean;
+  romanization: boolean;
+  reasons: string[];
+};
+
+export interface UseConjugationsVars {
+  stem: string;
+  isAdj: boolean;
+  honorific: boolean;
+  regular?: boolean;
+  conjugations?: string;
+}
+
+type ConjugationsResponse = {
+  conjugations: Conjugation[];
+};
+
+export interface UseConjugationsVars {
+  stem: string;
+  isAdj: boolean;
+  honorific: boolean;
+  regular?: boolean;
+  conjugations?: string;
+}
+
+const useConjugations = (
+  { stem, isAdj, honorific, regular, conjugations }: UseConjugationsVars,
+  options?: QueryHookOptions
+) => {
+  return useQuery<ConjugationsResponse>(CONJUGATIONS, {
+    ...options,
+    variables: {
+      stem: stem,
+      isAdj: isAdj,
+      honorific: honorific,
+      regular: regular,
+      conjugations: conjugations,
+    },
+  });
+};
+
+export default useConjugations;
