862716c39115c69aeccf9321b3e8306d540ae2f8
akash <akasheldo@hotmail.com>
2020-10-15 22:23:19 -0400

    Created app bar animations using custom layout

--- src/components/AppLayout/AppBar.tsx
+++ src/components/AppLayout/AppBar.tsx
@@ -1,36 +1,14 @@
-import React, { useState } from "react";
+import * as React from "react";
 import { Appbar, IconButton, TextInput } from "react-native-paper";
 import { StyleSheet, View } from "react-native";
+import { useState } from "react";
 import { useHistory } from "react-router";
+
+export const APP_BAR_HEIGHT = 56;
 
 export interface AppBarProps {
   title?: string;
 }
-
-const theme = {
-  colors: {
-    text: "white",
-    color: "white",
-    primary: "white",
-    background: "transparent",
-  },
-};
-
-const style = StyleSheet.create({
-  inputParent: {
-    flexGrow: 100,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  input: {
-    flexGrow: 100,
-    borderRadius: 0,
-    borderTopLeftRadius: 0,
-    borderTopRightRadius: 0,
-    height: 57,
-    overflow: "hidden",
-  },
-});
 
 const AppBar: React.FC<AppBarProps> = ({ title }) => {
   const [showSearch, setShowSearch] = useState(false);
@@ -44,7 +22,7 @@ const AppBar: React.FC<AppBarProps> = ({ title }) => {
   };
 
   return (
-    <Appbar.Header>
+    <Appbar.Header style={{ elevation: 0 }}>
       <Appbar.Content title={title} />
       {showSearch && (
         <View style={style.inputParent}>
@@ -75,5 +53,31 @@ const AppBar: React.FC<AppBarProps> = ({ title }) => {
     </Appbar.Header>
   );
 };
+
+const theme = {
+  colors: {
+    text: "white",
+    color: "white",
+    primary: "white",
+    background: "transparent",
+  },
+};
+
+const style = StyleSheet.create({
+  inputParent: {
+    flexGrow: 100,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    flexGrow: 100,
+    borderRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    height: APP_BAR_HEIGHT,
+    overflow: "hidden",
+  },
+  scrollView: { marginTop: APP_BAR_HEIGHT, flexGrow: 1 },
+});
 
 export default AppBar;
