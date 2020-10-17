57fc36d59221b06c2b5939001462bacc752ba9ab
akash <akasheldo@hotmail.com>
2020-10-17 12:46:16 -0400

    Created examples card and tests

---
+++ src/display/components/ExamplesCard.tsx
@@ -0,0 +1,28 @@
+import { List } from "react-native-paper";
+import ListItem from "../../components/ListItem";
+import BaseCard, { BaseCardProps } from "../../components/BaseCard";
+import React from "react";
+
+export interface ExamplesCardProps extends BaseCardProps {
+  examples: {
+    sentence: string;
+    translation: string;
+  }[];
+}
+
+const ExamplesCard: React.FC<ExamplesCardProps> = ({ examples, ...rest }) => (
+  <BaseCard title="Examples" {...rest}>
+    <List.Section>
+      {examples.map((example, index) => (
+        <ListItem
+          title={example.sentence}
+          description={example.translation}
+          key={index}
+          testID="examplesCardItem"
+        />
+      ))}
+    </List.Section>
+  </BaseCard>
+);
+
+export default ExamplesCard;
