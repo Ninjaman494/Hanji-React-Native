cf0a23584beca73607d2c3d936dffa721ca3be4c
akash <akasheldo@hotmail.com>
2020-10-21 18:10:47 -0400

    Created conj info card and its test

--- src/conj_info/ConjInfoPage.tsx
+++ src/conj_info/ConjInfoPage.tsx
@@ -1,81 +1,16 @@
 import React from "react";
-import { Text, Title, useTheme, Badge } from "react-native-paper";
 import AppLayout from "../components/AppLayout";
+import { Conjugation } from "../hooks/useConjugations";
+import ConjInfoCard from "./ConjInfoCard";
 //@ts-ignore
 import { useLocation } from "../routing";
-import BaseCard from "../components/BaseCard";
-import { Conjugation } from "../hooks/useConjugations";
-import { View } from "react-native";
 
 const ConjInfoPage: React.FC = () => {
   const conjugation: Conjugation = useLocation().state.conjugation;
-  const { colors, padding } = useTheme();
-
-  const styles = {
-    card: {
-      marginHorizontal: padding.horizontal,
-    },
-    badge: {
-      paddingHorizontal: padding.horizontal,
-      marginRight: padding.horizontal,
-      backgroundColor: colors.accent,
-    },
-    title: {
-      fontSize: 18,
-      color: colors?.primary,
-      marginTop: 16,
-      marginBottom: padding.vertical,
-    },
-    conjugation: {
-      fontSize: 24,
-      marginTop: -16,
-    },
-    text: {
-      fontSize: 20,
-    },
-    secondary: {
-      fontSize: 18,
-      color: "#808080",
-    },
-  };
-
-  const reasons = conjugation?.reasons.map((reason) => {
-    const index = reason.indexOf("(");
-    return {
-      type: reason.substring(0, index),
-      details: reason.substring(index),
-    };
-  });
 
   return (
     <AppLayout error={conjugation ? undefined : "Something went wrong"}>
-      <BaseCard
-        title={conjugation?.name}
-        style={styles.card}
-        rightIcon={() => (
-          <Badge
-            size={30}
-            visible={conjugation?.honorific}
-            style={styles.badge}
-          >
-            Honorific
-          </Badge>
-        )}
-      >
-        <View style={{ paddingHorizontal: padding.horizontal }}>
-          <Text style={styles.conjugation}>{conjugation?.conjugation}</Text>
-          <Title style={styles.title}>Pronunciation</Title>
-          <Text style={styles.text}>{conjugation?.pronunciation}</Text>
-          <Text style={styles.secondary}>{conjugation?.romanization}</Text>
-          <Title style={styles.title}>Explanation</Title>
-          {reasons.map((reason, index) => (
-            <View style={{ marginBottom: padding.vertical }} key={index}>
-              <Text style={styles.text}>{reason.type}</Text>
-              <Text style={styles.secondary}>{reason.details}</Text>
-            </View>
-          ))}
-        </View>
-      </BaseCard>
+      <ConjInfoCard conjugation={conjugation} />
     </AppLayout>
   );
 };
