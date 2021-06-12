af6da7ce09128bb583bed967dedd60ea17b8d5bb
akash <akasheldo@hotmail.com>
2021-06-12 10:02:46 -0400

    Improved FormikSelect styling and behavior

--- src/pages/favorites/AddFavoriteModal.tsx
+++ src/pages/favorites/AddFavoriteModal.tsx
@@ -3,15 +3,7 @@ import FormikSelect from "components/formikBindings/FormikSelect";
 import FormikTextField from "components/formikBindings/FormikTextField";
 import { Formik } from "formik";
 import React, { FC, ComponentProps, useState } from "react";
-import { useEffect } from "react";
-import { useRef } from "react";
-import {
-  Portal,
-  Dialog,
-  Paragraph,
-  Button,
-  useTheme,
-} from "react-native-paper";
+import { Portal, Dialog, Button } from "react-native-paper";
 
 export type AddFavoriteModalProps = Omit<
   ComponentProps<typeof Dialog>,
@@ -25,9 +17,6 @@ const AddFavoriteModal: FC<AddFavoriteModalProps> = ({
   onDismiss,
   onSubmit,
 }) => {
-  const [show, setVisible] = useState(false);
-  const [conjugation, setConjugation] = useState<number | undefined>(undefined);
-
   return (
     <Portal>
       <Dialog
@@ -36,35 +25,45 @@ const AddFavoriteModal: FC<AddFavoriteModalProps> = ({
         style={{ maxWidth: 500, width: "90%", alignSelf: "center" }}
       >
         <Dialog.Title>Create Favorite</Dialog.Title>
-        <Dialog.Content>
-          <Formik
-            initialValues={{ name: "", conjugation: "", honorific: false }}
-            onSubmit={() => {}}
-          >
-            {() => (
-              <FormikForm>
-                <FormikTextField label="Name" />
-                <FormikSelect
-                  label="Conjugation"
-                  visible={show}
-                  showDropDown={() => setVisible(true)}
-                  onDismiss={() => setVisible(false)}
-                  value={conjugation}
-                  setValue={(v) => setConjugation(v as number)}
-                  list={[
-                    { label: "connective and", value: 1 },
-                    { label: "connective but", value: 2 },
-                    { label: "declarative past", value: 3 },
-                  ]}
-                />
-              </FormikForm>
-            )}
-          </Formik>
-        </Dialog.Content>
-        <Dialog.Actions>
-          <Button onPress={onDismiss}>Cancel</Button>
-          <Button onPress={onSubmit}>Submit</Button>
-        </Dialog.Actions>
+        <Formik
+          initialValues={{ name: "", conjugation: "", honorific: false }}
+          onSubmit={(v) => {
+            console.log(v);
+            onSubmit();
+          }}
+        >
+          {({ handleSubmit }) => (
+            <>
+              <Dialog.Content>
+                <FormikForm>
+                  <FormikTextField name="name" label="Name" />
+                  <FormikSelect
+                    name="conjugation"
+                    label="Conjugation"
+                    list={[
+                      { label: "connective and", value: "CONNECTIVE_AND" },
+                      { label: "connective but", value: "CONNECTIVE_BUT" },
+                      { label: "declarative past", value: "DECLARATIVE_PAST" },
+                    ]}
+                  />
+                  <FormikSelect
+                    name="tense"
+                    label="Tense"
+                    list={[
+                      { label: "and", value: "and" },
+                      { label: "but", value: "but" },
+                      { label: "past", value: "past" },
+                    ]}
+                  />
+                </FormikForm>
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button onPress={onDismiss}>Cancel</Button>
+                <Button onPress={handleSubmit}>Submit</Button>
+              </Dialog.Actions>
+            </>
+          )}
+        </Formik>
       </Dialog>
     </Portal>
   );
