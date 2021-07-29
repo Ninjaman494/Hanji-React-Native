66c28856f9f334e83d6bb866cf7e2a96c3f42fed
akash <akasheldo@hotmail.com>
2021-07-28 20:25:38 -0400

    Re-did with native rating library

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ import ConjInfoPage from "pages/conjInfo/ConjInfoPage";
 import SettingsPage from "pages/settings/SettingsPage";
 import FavoritesPage from "pages/favorites/FavoritesPage";
 import AcknowledgementsPage from "pages/settings/AcknowledgementsPage";
-import RatingDialog from "components/RatingDialog";
+import RatingHandler from "components/RatingHandler";
 
 const client = new ApolloClient({
   uri: "https://hanji-server.appspot.com/graphql",
@@ -48,7 +48,7 @@ export default function Index(): JSX.Element {
           barStyle="default"
         />
         <View style={styles.parent}>
-          <RatingDialog numSessions={3} />
+          <RatingHandler numSessions={3} />
           <View style={styles.container}>
             <Router>
               <Switch>
