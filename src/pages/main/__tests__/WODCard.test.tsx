2304c677e5c08c706d6a2a5b3c0109fc3280177b
akash <akasheldo@hotmail.com>
2021-07-31 18:35:59 -0400

    Created WODCard test

---
+++ src/pages/main/__tests__/WODCard.test.tsx
@@ -0,0 +1,68 @@
+jest.mock("react-router");
+jest.mock("hooks/useGetWOD");
+
+import "react-native";
+import React from "react";
+import { fireEvent, render } from "@testing-library/react-native";
+import { useHistory } from "react-router";
+import WODCard from "../WODCard";
+import useGetWOD from "hooks/useGetWOD";
+
+const pushHistory = jest.fn();
+(useHistory as jest.Mock).mockReturnValue({
+  push: pushHistory,
+});
+
+describe("WODCard component", () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  it("shows Word of the Day", () => {
+    (useGetWOD as jest.Mock).mockReturnValue({
+      loading: false,
+      error: null,
+      data: {
+        wordOfTheDay: {
+          id: "abc1",
+          term: "foo",
+        },
+      },
+    });
+    const result = render(<WODCard />);
+
+    expect(result.getByText("foo")).toBeTruthy();
+
+    fireEvent.press(result.getByText("See entry"));
+
+    expect(pushHistory).toHaveBeenCalledWith("/display?id=abc1");
+  });
+
+  it("handles loading state", () => {
+    (useGetWOD as jest.Mock).mockReturnValue({
+      loading: true,
+      error: null,
+      data: null,
+    });
+    const result = render(<WODCard />);
+
+    expect(result.getByText("Loading...")).toBeTruthy();
+
+    fireEvent.press(result.getByText("See entry"));
+
+    expect(pushHistory).not.toHaveBeenCalled();
+  });
+
+  it("handles error state", () => {
+    (useGetWOD as jest.Mock).mockReturnValue({
+      loading: false,
+      error: true,
+      data: null,
+    });
+    const result = render(<WODCard />);
+
+    expect(result.getByText("Could not fetch Word of the Day")).toBeTruthy();
+
+    fireEvent.press(result.getByText("See entry"));
+
+    expect(pushHistory).not.toHaveBeenCalled();
+  });
+});
