dac533c5cb37fa3efd5b9801a96764e1f878b2ed
akash <akasheldo@hotmail.com>
2021-08-08 15:57:40 -0400

    Show image and basic form

--- src/pages/bugReport/BugReportPage.tsx
+++ src/pages/bugReport/BugReportPage.tsx
@@ -1,15 +1,58 @@
-import { AppLayout } from "components";
+import { AppBar, FormikForm, FormikTextField } from "components";
+import { Formik } from "formik";
 import React, { FC } from "react";
-import { Text } from "react-native-paper";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useTheme } from "react-native-paper";
 import { useLocation } from "react-router-native";
 
 const BugReportPage: FC = () => {
+  const { padding, colors } = useTheme();
   const uri = useLocation<{ screenshot: string }>().state.screenshot;
 
+  const styles = StyleSheet.create({
+    form: {
+      paddingHorizontal: padding.horizontal,
+      paddingVertical: padding.vertical,
+    },
+    imageContainer: {
+      flex: 1,
+      flexDirection: "row",
+      alignContent: "flex-start",
+      marginHorizontal: padding.horizontal,
+    },
+    image: {
+      flex: 1,
+      height: 400,
+      borderColor: colors.grey,
+      borderWidth: 1,
+      backgroundColor: colors.grey,
+      resizeMode: "contain",
+    },
+    imageText: {
+      flex: 1,
+      marginStart: padding.horizontal,
+    },
+  });
+
   return (
-    <AppLayout>
-      <Text>{uri}</Text>
-    </AppLayout>
+    <>
+      <AppBar title="Report a Bug" />
+      <ScrollView>
+        <Formik initialValues={{ feedback: "", email: "" }} onSubmit={() => {}}>
+          <FormikForm style={styles.form}>
+            <FormikTextField name="feedback" label="Feedback" multiline />
+            <FormikTextField name="email" label="Email (optional)" />
+          </FormikForm>
+        </Formik>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{ uri }} />
+          <Text style={styles.imageText}>
+            The screenshot allows us to have more contextual information about
+            your feedback
+          </Text>
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
