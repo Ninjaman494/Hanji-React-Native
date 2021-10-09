b9bb03a272e875750a13a0fa954b5f7588cee2c5
akash <akasheldo@hotmail.com>
2021-10-09 10:19:36 -0400

    Created ErrorDialog and used across app

---
+++ src/components/ErrorDialog.tsx
@@ -0,0 +1,24 @@
+import { ApolloError } from "@apollo/client";
+import React, { ComponentProps, FC } from "react";
+import { Button, Dialog, Text } from "react-native-paper";
+
+export interface ErrorDialog
+  extends Omit<ComponentProps<typeof Dialog>, "children"> {
+  error: ApolloError;
+}
+
+const ErrorDialog: FC<ErrorDialog> = ({ error, ...rest }) => {
+  return (
+    <Dialog {...rest}>
+      <Dialog.Title>An Error Occurred</Dialog.Title>
+      <Dialog.Content>
+        <Text>Error: {JSON.stringify(error, null, 2)}</Text>
+      </Dialog.Content>
+      <Dialog.Actions>
+        <Button onPress={rest.onDismiss}>OK</Button>
+      </Dialog.Actions>
+    </Dialog>
+  );
+};
+
+export default ErrorDialog;
