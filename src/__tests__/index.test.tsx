1f704a327c796daee3236800b584f6449d0e398b
akash <akasheldo@hotmail.com>
2021-10-20 21:31:17 -0400

    Created tests for Sentry and global error handlers

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -11,18 +11,29 @@ import Index from "index";
 import { DEFAULT_FAVORITES } from "pages/main/MainPage";
 import React from "react";
 import "react-native";
+import {
+  setJSExceptionHandler,
+  setNativeExceptionHandler,
+} from "react-native-exception-handler";
 import { useLocation } from "react-router";
+import { init, Native } from "sentry-expo";
 
 jest.useFakeTimers();
 
-(useLocation as jest.Mock).mockReturnValue({
-  pathname: "path",
-});
+const pathname = "path";
+(useLocation as jest.Mock).mockReturnValue({ pathname });
 
 const setFavorites = jest.fn();
 (useSetFavorites as jest.Mock).mockReturnValue({ setFavorites });
 
 describe("Index", () => {
+  beforeEach(() => {
+    (useGetFavorites as jest.Mock).mockReturnValue({
+      loading: false,
+      favorites: DEFAULT_FAVORITES,
+    });
+  });
+
   it("creates favorites if none exist", async () => {
     (useGetFavorites as jest.Mock).mockReturnValue({
       loading: false,
@@ -36,15 +47,33 @@ describe("Index", () => {
   });
 
   it("doesn't create favorites if they already exist", async () => {
-    (useGetFavorites as jest.Mock).mockReturnValue({
-      loading: false,
-      favorites: DEFAULT_FAVORITES,
-    });
     setFavorites.mockClear();
 
     render(<Index />);
 
     expect(useGetFavorites).toHaveBeenCalled();
     expect(setFavorites).not.toHaveBeenCalled();
+  });
+
+  it("initializes Sentry", async () => {
+    render(<Index />);
+
+    expect(init).toHaveBeenCalled();
+    expect(Native.setContext).toHaveBeenCalledWith("Favorites", {
+      favorites: DEFAULT_FAVORITES,
+    });
+    expect(Native.addBreadcrumb).toHaveBeenCalledWith({
+      category: "navigation",
+      message: `Route changed to ${pathname}`,
+      level: Native.Severity.Info,
+      data: { pathname },
+    });
+  });
+
+  it("sets global error handlers", () => {
+    render(<Index />);
+
+    expect(setJSExceptionHandler).toHaveBeenCalled();
+    expect(setNativeExceptionHandler).toHaveBeenCalled();
   });
 });
