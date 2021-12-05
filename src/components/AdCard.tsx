b94f3522f8fcc8e8bafc2b08bc1914d761ddcfb6
akash <akasheldo@hotmail.com>
2021-12-05 15:23:38 -0500

    Hide ads once purchased

--- src/components/AdCard.tsx
+++ src/components/AdCard.tsx
@@ -1,14 +1,25 @@
 import { AdMobBanner } from "expo-ads-admob";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "react-native";
+import Purchases from "react-native-purchases";
 import BaseCard, { BaseCardProps } from "./BaseCard";
 
 export interface AdCardProps extends BaseCardProps {
   adUnitID: string;
 }
 
-const AdCard = ({ adUnitID, ...rest }: AdCardProps): JSX.Element => {
-  return (
+const AdCard = ({ adUnitID, ...rest }: AdCardProps): JSX.Element | null => {
+  // Hide ad until we know user isn't ad-free
+  const [showAd, setShowAd] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { entitlements } = await Purchases.getPurchaserInfo();
+      setShowAd(!entitlements.active.ad_free_entitlement);
+    })();
+  }, [setShowAd]);
+
+  return showAd ? (
     <BaseCard title="Ad" {...rest}>
       <AdMobBanner
         bannerSize="mediumRectangle"
@@ -17,7 +28,7 @@ const AdCard = ({ adUnitID, ...rest }: AdCardProps): JSX.Element => {
         style={{ alignSelf: "center" }}
       />
     </BaseCard>
-  );
+  ) : null;
 };
 
 export default AdCard;
