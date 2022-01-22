dc17e072838a01255a35f8a4519d4ea0203ec038
Ninjaman494 <akasheldo@hotmail.com>
2022-01-21 19:10:18 -0500

    Error Dialog should have more specific error messages (#67)
    * Better error message and ignore network errors
    
    * Created test and fixed edge case

---
+++ src/components/__tests__/ErrorDialog.test.tsx
@@ -0,0 +1,49 @@
+jest.mock("sentry-expo");
+
+import { ApolloError } from "@apollo/client";
+import React from "react";
+import { Native } from "sentry-expo";
+import { render } from "utils/testUtils";
+import ErrorDialog from "../ErrorDialog";
+
+describe("ErrorDialog component", () => {
+  it("reports errors to Sentry", () => {
+    const error = new ApolloError({});
+    const result = render(<ErrorDialog visible error={error} />);
+
+    expect(Native.captureException).toHaveBeenCalledWith(error, {
+      extra: { error },
+    });
+    expect(
+      result.queryByText(
+        "An error occurred. Please try again later or contact support."
+      )
+    ).toBeTruthy();
+  });
+
+  it("doesn't report network errors to Sentry", () => {
+    const error = new ApolloError({
+      networkError: new Error("Network request failed"),
+    });
+    const result = render(<ErrorDialog visible error={error} />);
+
+    expect(Native.captureException).not.toHaveBeenCalled();
+    expect(
+      result.queryByText(
+        "Network request failed. Please try again later or contact support."
+      )
+    ).toBeTruthy();
+  });
+
+  it("uses error message if present", () => {
+    const error = new ApolloError({ errorMessage: "foobar" });
+    const result = render(<ErrorDialog visible error={error} />);
+
+    expect(Native.captureException).toHaveBeenCalledWith(error, {
+      extra: { error },
+    });
+    expect(
+      result.queryByText("foobar. Please try again later or contact support.")
+    ).toBeTruthy();
+  });
+});
