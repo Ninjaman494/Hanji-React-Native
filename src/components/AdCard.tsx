06711b91eb4d8bac70841b1e3c6cfc753765525e
Ninjaman494 <akasheldo@hotmail.com>
2022-03-12 16:44:28 -0500

    Use display=none for ads and Added ad error handler (#76)
    * Changed adcard to use display=none
    
    * Created admob error handler
    
    * Added typecheck to CI

--- src/components/AdCard.tsx
+++ src/components/AdCard.tsx
@@ -2,24 +2,30 @@ import { AdMobBanner } from "expo-ads-admob";
 import useGetAdFreeStatus from "hooks/useGetAdFreeStatus";
 import React from "react";
 import "react-native";
-import { Native } from "sentry-expo";
+import handleAdError from "utils/handleAdError";
 import BaseCard, { BaseCardProps } from "./BaseCard";
 
 export interface AdCardProps extends BaseCardProps {
   adUnitID: string;
 }
 
-const AdCard = ({ adUnitID, ...rest }: AdCardProps): JSX.Element | null => {
+const AdCard = ({
+  adUnitID,
+  style,
+  ...rest
+}: AdCardProps): JSX.Element | null => {
   const { isAdFree } = useGetAdFreeStatus();
 
-  return isAdFree ? null : (
-    <BaseCard title="Ad" {...rest}>
+  return (
+    <BaseCard
+      title="Ad"
+      style={[style, { display: !isAdFree ? "none" : undefined }]}
+      {...rest}
+    >
       <AdMobBanner
         bannerSize="mediumRectangle"
         adUnitID={adUnitID}
-        onDidFailToReceiveAdWithError={(error) =>
-          Native.captureException(error, { extra: { error } })
-        }
+        onDidFailToReceiveAdWithError={handleAdError}
         style={{ alignSelf: "center" }}
       />
     </BaseCard>
