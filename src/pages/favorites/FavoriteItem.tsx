c11c8862b3ea5785823e37726bf0533741fc8d31
akash <akasheldo@hotmail.com>
2022-03-15 19:00:41 -0400

    Created delete favorite modal with btn

--- src/pages/favorites/FavoriteItem.tsx
+++ src/pages/favorites/FavoriteItem.tsx
@@ -1,8 +1,8 @@
 import { HonorificBadge } from "components";
 import { Favorite } from "hooks/useGetFavorites";
-import React, { FC, useState } from "react";
-import { StyleSheet } from "react-native";
-import { Divider, List, Menu, useTheme } from "react-native-paper";
+import React, { FC } from "react";
+import { StyleSheet, View } from "react-native";
+import { Divider, IconButton, List, useTheme } from "react-native-paper";
 
 export interface FavoriteItemProps {
   favorite: Favorite;
@@ -10,8 +10,6 @@ export interface FavoriteItemProps {
 }
 
 const FavoriteItem: FC<FavoriteItemProps> = ({ favorite, onDelete }) => {
-  const [showMenu, setShowMenu] = useState(false);
-
   const { textSizes } = useTheme();
   const styles = StyleSheet.create({
     title: { fontSize: textSizes.regular, marginBottom: 4 },
@@ -23,37 +21,31 @@ const FavoriteItem: FC<FavoriteItemProps> = ({ favorite, onDelete }) => {
   });
 
   return (
-    <Menu
-      visible={showMenu}
-      onDismiss={() => setShowMenu(false)}
-      anchor={
-        <>
-          <List.Item
-            onPress={() => {}}
-            onLongPress={() => setShowMenu(true)}
-            title={favorite.name}
-            description={favorite.conjugationName}
-            titleStyle={styles.title}
-            descriptionStyle={styles.description}
-            right={() => (
-              <HonorificBadge
-                visible={favorite.honorific}
-                style={styles.badge}
-              />
-            )}
-          />
-          <Divider />
-        </>
-      }
-    >
-      <Menu.Item
-        title="Delete"
-        onPress={() => {
-          onDelete(favorite);
-          setShowMenu(false);
-        }}
+    <>
+      <List.Item
+        title={favorite.name}
+        description={favorite.conjugationName}
+        titleStyle={styles.title}
+        descriptionStyle={styles.description}
+        right={() => (
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <HonorificBadge visible={favorite.honorific} style={styles.badge} />
+            <IconButton
+              icon="trash-can"
+              size={24}
+              onPress={() => onDelete(favorite)}
+            />
+          </View>
+        )}
       />
-    </Menu>
+      <Divider />
+    </>
   );
 };
 
