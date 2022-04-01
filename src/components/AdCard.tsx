ab445e388165ce3e084d4efe3115cf22ff4342ff
akash <akasheldo@hotmail.com>
2022-04-01 18:49:10 -0400

    Revert "Use display=none for ads and Added ad error handler (#76)"
    This reverts commit 06711b91eb4d8bac70841b1e3c6cfc753765525e.

--- src/components/AdCard.tsx
+++ src/components/AdCard.tsx
@@ -2,30 +2,24 @@ import { AdMobBanner } from "expo-ads-admob";
 import useGetAdFreeStatus from "hooks/useGetAdFreeStatus";
 import React from "react";
 import "react-native";
-import handleAdError from "utils/handleAdError";
+import { Native } from "sentry-expo";
 import BaseCard, { BaseCardProps } from "./BaseCard";
 
 export interface AdCardProps extends BaseCardProps {
   adUnitID: string;
 }
 
-const AdCard = ({
-  adUnitID,
-  style,
-  ...rest
-}: AdCardProps): JSX.Element | null => {
+const AdCard = ({ adUnitID, ...rest }: AdCardProps): JSX.Element | null => {
   const { isAdFree } = useGetAdFreeStatus();
 
-  return (
-    <BaseCard
-      title="Ad"
-      style={[style, { display: !isAdFree ? "none" : undefined }]}
-      {...rest}
-    >
+  return isAdFree ? null : (
+    <BaseCard title="Ad" {...rest}>
       <AdMobBanner
         bannerSize="mediumRectangle"
         adUnitID={adUnitID}
-        onDidFailToReceiveAdWithError={handleAdError}
+        onDidFailToReceiveAdWithError={(error) =>
+          Native.captureException(error, { extra: { error } })
+        }
         style={{ alignSelf: "center" }}
       />
     </BaseCard>
