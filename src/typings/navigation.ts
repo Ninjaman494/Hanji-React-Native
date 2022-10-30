a4c07d4ff61cd7f7e7479bd12cb2e311d67e31e8
Akash Eldo <akasheldo@hotmail.com>
2022-10-30 14:09:05 -0400

    Removed add button and suggestions page

--- src/typings/navigation.ts
+++ src/typings/navigation.ts
@@ -12,7 +12,6 @@ export type StackParamList = {
   Acknowledgements: undefined;
   BugReport: { screenshot: string };
   Conjugator: { term: string };
-  Suggestion: { entryId: string };
   Survey: undefined;
 };
 
