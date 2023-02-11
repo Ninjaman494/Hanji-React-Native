307f6727216edc02310dd6282a13562006dbd61b
Akash Eldo <akasheldo@hotmail.com>
2023-02-11 17:52:59 -0500

    Added shadow styling

--- src/components/HintTooltip.tsx
+++ src/components/HintTooltip.tsx
@@ -10,11 +10,35 @@ interface HintTooltipProps extends TooltipProps {
 const HintTooltip: FC<HintTooltipProps> = ({ text, ...rest }) => {
   return (
     <Tooltip
+      disableShadow
       useInteractionManager
       placement="top"
       backgroundColor="rgba(0,0,0,0)"
       content={<Text>{text}</Text>}
-      contentStyle={{ backgroundColor: "#BABABA" }}
+      arrowSize={{ width: 24, height: 12 }}
+      contentStyle={{
+        backgroundColor: "#F3F5FE",
+        elevation: 4,
+      }}
+      tooltipStyle={{
+        shadowColor: "#000000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+      }}
+      arrowStyle={{
+        elevation: 4,
+        shadowColor: "#FFFFFF",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+      }}
       topAdjustment={
         Platform.OS === "android" ? -(StatusBar.currentHeight ?? 0) : 0
       }
--- src/pages/display/components/FavoritesCard.tsx
+++ src/pages/display/components/FavoritesCard.tsx
@@ -109,7 +109,7 @@ const FavoritesCard: React.FC<FavoritesCardProps> = ({
         <HintTooltip
           isVisible={showPopup}
           onClose={() => setShowPopup(false)}
-          text="Hello World!"
+          text="See more conjugations!"
         >
           <Button onPress={onPress} color={colors.accent}>
             See all
