4b786e3c281e0f265078326d628ee7f6bb6e86a6
Akash Eldo <akasheldo@hotmail.com>
2025-01-29 18:04:56 -0500

    Added a console.log for showing ad

--- src/components/AdCard.tsx
+++ src/components/AdCard.tsx
@@ -2,7 +2,6 @@ import { useNavigation } from "@react-navigation/native";
 import useUserContext from "hooks/useUserContext";
 import React, { useEffect, useState } from "react";
 import "react-native";
-import { View } from "react-native";
 import { AppodealMrec } from "react-native-appodeal";
 import { NavigationProps } from "typings/navigation";
 import BaseCard, { BaseCardProps } from "./BaseCard";
@@ -15,7 +14,10 @@ const AdCard = (props: BaseCardProps): JSX.Element | null => {
   useEffect(() => {
     if (isAdFree) return;
 
-    const updateBannerVisibility = () => setShowAd(true);
+    const updateBannerVisibility = () => {
+      setShowAd(true);
+      console.log("[HANJI AD] Showing ad");
+    };
 
     navigation.addListener("transitionEnd", updateBannerVisibility);
 
@@ -26,22 +28,20 @@ const AdCard = (props: BaseCardProps): JSX.Element | null => {
 
   return isAdFree ? null : (
     <BaseCard title="Ad" testID="adCardBase" {...props}>
-      {showAd ? (
-        <AppodealMrec
-          style={{
-            height: 250,
-            width: "100%",
-            alignContent: "stretch",
-            alignSelf: "center",
-          }}
-          onAdLoaded={() => console.log("Banner view did load")}
-          onAdExpired={() => console.log("Banner view expired")}
-          onAdClicked={() => console.log("Banner view is clicked")}
-          onAdFailedToLoad={() => console.log("Banner view failed to load")}
-        />
-      ) : (
-        <View style={{ height: 250 }} />
-      )}
+      <AppodealMrec
+        style={{
+          height: 250,
+          width: "100%",
+          alignContent: "stretch",
+          alignSelf: "center",
+        }}
+        onAdLoaded={() => console.log("[HANJI AD] Banner view did load")}
+        onAdExpired={() => console.log("[HANJI AD] Banner view expired")}
+        onAdClicked={() => console.log("[HANJI AD] Banner view is clicked")}
+        onAdFailedToLoad={() =>
+          console.log("[HANJI AD] Banner view failed to load")
+        }
+      />
     </BaseCard>
   );
 };
