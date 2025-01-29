7226e064281e72869eb51f81a3641185ea6b2190
Akash Eldo <akasheldo@hotmail.com>
2025-01-29 18:16:17 -0500

    Reset logic back to original

--- src/components/AdCard.tsx
+++ src/components/AdCard.tsx
@@ -2,6 +2,7 @@ import { useNavigation } from "@react-navigation/native";
 import useUserContext from "hooks/useUserContext";
 import React, { useEffect, useState } from "react";
 import "react-native";
+import { View } from "react-native";
 import { AppodealMrec } from "react-native-appodeal";
 import { NavigationProps } from "typings/navigation";
 import BaseCard, { BaseCardProps } from "./BaseCard";
@@ -28,20 +29,24 @@ const AdCard = (props: BaseCardProps): JSX.Element | null => {
 
   return isAdFree ? null : (
     <BaseCard title="Ad" testID="adCardBase" {...props}>
-      <AppodealMrec
-        style={{
-          height: 250,
-          width: "100%",
-          alignContent: "stretch",
-          alignSelf: "center",
-        }}
-        onAdLoaded={() => console.log("[HANJI AD] Banner view did load")}
-        onAdExpired={() => console.log("[HANJI AD] Banner view expired")}
-        onAdClicked={() => console.log("[HANJI AD] Banner view is clicked")}
-        onAdFailedToLoad={() =>
-          console.log("[HANJI AD] Banner view failed to load")
-        }
-      />
+      {showAd ? (
+        <AppodealMrec
+          style={{
+            height: 250,
+            width: "100%",
+            alignContent: "stretch",
+            alignSelf: "center",
+          }}
+          onAdLoaded={() => console.log("[HANJI AD] Banner view did load")}
+          onAdExpired={() => console.log("[HANJI AD] Banner view expired")}
+          onAdClicked={() => console.log("[HANJI AD] Banner view is clicked")}
+          onAdFailedToLoad={() =>
+            console.log("[HANJI AD] Banner view failed to load")
+          }
+        />
+      ) : (
+        <View style={{ height: 250 }} />
+      )}
     </BaseCard>
   );
 };
